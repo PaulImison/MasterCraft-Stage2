adc2ae97edde94e9bda7cba510bdbe1c9b3ef25d
Paul Imison <paulimison@gmail.com>
2025-06-07 23:09:52 +0300

    Use PostgreSql

--- payments_api/settings.py
+++ payments_api/settings.py
@@ -15,6 +15,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,13 +82,18 @@ WSGI_APPLICATION = 'payments_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback if DATABASE_URL not set
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
