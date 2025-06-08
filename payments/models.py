2400edc8ed3f8093c7aca449ad9258f83994963b
Paul Imison <paulimison@gmail.com>
2025-06-08 14:19:17 +0300

    Removed Kshs reference

--- payments/models.py
+++ payments/models.py
@@ -5,13 +5,13 @@ class PaymentTransaction(models.Model):
     reference = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    amount = models.IntegerField(help_text="Amount in kshs * 100")
+    amount = models.IntegerField(help_text="Amount in kobo")
     status = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.email} - {self.reference} - {self.status}"
     
-    def amount_in_kshs(self):
-        return self.amount / 100
+    # def amount_in_kshs(self):
+    #     return self.amount / 100
 
